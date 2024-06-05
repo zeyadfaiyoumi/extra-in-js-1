004574af55ffb1563d03a10bd8c6a2dd4ef9bea1
zeyadfaiyoumi <zeyadfaiyoumi@gmail.com>
2024-06-05 06:38:57 +0300

    extra-in-js-1

---
+++ newwwwww.js
@@ -0,0 +1,57 @@
+let num1=prompt("enter num1")
+alert("num1="+num1)
+
+var numOne=prompt("enter numOne")
+var num2=prompt("enter num2")
+if(numOne>num2){
+alert(num2+"   "+numOne)}
+else{alert(numOne+"   "+num2)}
+let inputs = parseInt(prompt("please enter numberss"))
+switch(inputs){
+case 1: 
+    alert("one")
+    break;
+    case 2:
+    alert("tow")
+    break;
+    case 3:
+        alert("THREE")
+        break;
+        case 4:
+            alert("FOUR")
+            break;
+            case 5:
+                alert("FIVE")
+                break;
+                case 6:
+                    alert("SIX")
+                    break;
+                    case 7:
+                        alert("SEVEN")
+                        break;
+                        case 8:
+                            alert("EIGHT")
+                            break;
+                            case 9:
+                                alert("NINE")
+                                break;
+                                                                                 
+                                default:
+                                    alert("PLEASE TRY AGAIN")
+}
+
+console.log(5*3)
+console.log("JavaScript"+ 50 )
+console.log(17 % 5 )
+console.log(5 % 17)
+console.log(5/10)
+console.log(4 === "4")
+console.log(4 != 5)
+console.log(7 <= 8)
+console.log("Hello" + 5)
+console.log(Math.ceil(x) - Math.floor(x) )
+console.log(Math.pow(x,2))
+
+
+
+
